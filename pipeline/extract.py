07198b67f94d78ad0e8d641bb18292d6a906bdc1
Jemarco Briz <jemarcobriz123@gmail.com>
2026-08-05 23:43:14 +0800

    feat: implement csv to dataframe extraction

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -2,7 +2,14 @@
 # Input: daset.csv
 # Output: Pandas DataFrame
 import pandas as pd 
+from pandas import DataFrame
 
-def read_csv():
-    ...
 
+def extract_csv(file_path: str) -> DataFrame:
+    df = pd.read_csv(file_path)
+    print(df)
+    return df
+
+
+
+extract_csv("data/dataset.csv")
