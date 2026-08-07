46801672af757104340f08c156d50199e57d363c
Jemarco Briz <jemarcobriz123@gmail.com>
2026-08-07 12:21:58 +0800

    feat: load YAML into python dictionary for data validation

--- pipeline/validation.py
+++ pipeline/validation.py
@@ -1,6 +1,20 @@
 #purpose: validate the extracted data
 #continue if valid, raise exception if not valid 
+import pandas as pd 
+import yaml
+import logging
 
-def validate_schema():
-    ...
+from pipeline.extract import extract_csv
+from config.paths import EMAILS_SCHEMA_PATH
+from config.paths import EMAILS_DATASET_PATH
+def validate_schema(df, schema_path):
+    
+    #load the YAML file into python dictionary
+    with open(schema_path) as file:
+        schema = yaml.safe_load(file) 
+    print(schema)
+
+
+if __name__ == "__main__":
+    validate_schema(extract_csv(EMAILS_DATASET_PATH), EMAILS_SCHEMA_PATH)
     
