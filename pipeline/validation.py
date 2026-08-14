60653f873fffbd884917a6812ee08e08ff5a9b59
Jemarco Briz <jemarcobriz123@gmail.com>
2026-08-14 19:07:26 +0800

    refactor: add type hints

--- pipeline/validation.py
+++ pipeline/validation.py
@@ -1,14 +1,16 @@
 #purpose: validate the extracted data
 #continue if valid, raise exception if not valid 
 import pandas as pd 
+from pandas import DataFrame
 import yaml
 import logging
-
+from pathlib import Path
 from pipeline.extract import extract_csv
 from config.paths import EMAILS_SCHEMA_PATH
 from config.paths import EMAILS_DATASET_PATH
 from pprint import pprint
-def validate_df_schema(df, schema_path):
+
+def validate_df_schema(df: DataFrame, schema_path: Path) ->None:
     
     #load the YAML file into python dictionary
     schema = load_YAML_to_dict(schema_path)
@@ -19,11 +21,11 @@ def validate_df_schema(df, schema_path):
 
 
 
-def load_YAML_to_dict(schema_path):
+def load_YAML_to_dict(schema_path: Path) -> dict:
     with open(schema_path) as file:
         schema = yaml.safe_load(file)
     return  schema
-def validate_column_names(df, schema):
+def validate_column_names(df: DataFrame, schema: dict) ->None: 
      #Expected Columns
     expected_columns = { #extract column names from the schema columns
         column["name"]
@@ -36,7 +38,7 @@ def validate_column_names(df, schema):
     missing_columns = expected_columns - actual_columns
     unexpected_columns = actual_columns - expected_columns
 
-
+    
     #validate column names
     if missing_columns or unexpected_columns:
         raise ValueError(
@@ -47,8 +49,8 @@ def validate_column_names(df, schema):
     else:
         print("Column names validation passed")
 
-def validate_column_nullability(df, schema): #validate nullability of each columns in the df
-    ...
+def validate_column_nullability(df: DataFrame, schema: dict) ->None : #validate nullability of each columns in the df
+    ... 
 
 if __name__ == "__main__":
     validate_df_schema(extract_csv(EMAILS_DATASET_PATH), EMAILS_SCHEMA_PATH)
